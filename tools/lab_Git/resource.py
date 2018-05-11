3ae2a6e00bbf18f1f391500a333ee8fc473c92da
Shuoqi Zhou <39119366+zhoushuoqi@users.noreply.github.com>
2018-05-11 09:23:04 +0800

    Update resource.py

--- tools/lab_Git/resource.py
+++ tools/lab_Git/resource.py
@@ -3,16 +3,16 @@ import paramiko
 
 
      
-#choose = raw_input(' 1. GA 768 \n 2. GA 1080 \n 3. GB 768 \n 4. GB 1080 \n Any other press for quit:\n Input: ')
+#choose = raw_input('**** ')
 
 
 def serverConnect(node_ip):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())   
-    ssh.connect(hostname=node_ip, port=22, username='mats', password='cienet')
+    ssh.connect(hostname=node_ip, port=22, username='****', password='****')
     return ssh
 #ssh = serverConnect()
-#stdin, stdout, stderr = ssh.exec_command('ls /cygdrive/c/Users/mats/MATS')
+#stdin, stdout, stderr = ssh.exec_command('****')
 #res_list = stdout.readlines()
 #print res_list
 #print stdout.read()
@@ -26,10 +26,10 @@ def fun2(path,path_image):
         node_ip = '10.100.29.%d'%(node)
         try:
             ssh = serverConnect(node_ip)
-            stdin, stdout, stderr = ssh.exec_command('ls /cygdrive/c/gitdata/info3/my20-Intel-768P/audio')
-            stdin2, stdout2, stderr2 = ssh.exec_command('ls /cygdrive/c/gitdata/info3/%s/%s'%(path,path_image))
-            stdin3, stdout3, stderr3 = ssh.exec_command('ls /cygdrive/c/gitdata/info3/%s/operations'%(path))
-            stdin4, stdout4, stderr4 = ssh.exec_command('ls /cygdrive/c/Users/mats/MATS/python')
+            stdin, stdout, stderr = ssh.exec_command('ls ****')
+            stdin2, stdout2, stderr2 = ssh.exec_command('ls ****%(path,path_image))
+            stdin3, stdout3, stderr3 = ssh.exec_command('ls ****'%(path))
+            stdin4, stdout4, stderr4 = ssh.exec_command('ls ****')
             res_list_audio = stdout.readlines()
             res_list_image = stdout2.readlines()
             res_list_operation = stdout3.readlines()
